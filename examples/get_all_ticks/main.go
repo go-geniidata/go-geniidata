b963834ec6adc178a9ff5f99b4eaa5fd542462b3
alex-x-crypto <67682756+alex-x-crypto@users.noreply.github.com>
2023-12-05 16:44:48 +0800

    update example

--- examples/get_all_ticks/main.go
+++ examples/get_all_ticks/main.go
@@ -19,29 +19,42 @@ func main() {
 	defer cancel()
 
 	fmt.Println("tick,deployer,deployTime,decimals,max,limit,holders,transactions,minted,mintProgress")
-	for {
-		req := brc20.RequestTickList{
-			Limit:  limit,
-			Offset: offset,
-		}
-		resp, err := b.TickList(ctx, req)
-		if err != nil {
-			log.Fatalf("http request error: %s", err)
-		}
-		//fmt.Printf("count: %d\n", resp.Data.Count)
-
-		for _, t := range resp.Data.List {
-			fmt.Printf(
-				"%s,%s,%s,%d,%s,%s,%d,%d,%s,%s\n",
-				t.Tick, t.Deployer, time.Unix(t.DeployTime, 0),
-				t.Decimals, t.Max, t.Limit, t.Holders, t.Transactions,
-				t.Minted, t.MintProgress,
-			)
-		}
-
-		offset += limit
-		if offset >= resp.Data.Count {
-			break
+	count, err := doWork(ctx, b, limit, offset)
+	if err != nil {
+		log.Fatalf("http request error: %s", err)
+	}
+	offset += limit
+	for offset < count {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(time.Minute):
+			count, err = doWork(ctx, b, limit, offset)
+			if err != nil {
+				log.Fatalf("http request error: %s", err)
+			}
+			offset += limit
 		}
 	}
+}
+
+func doWork(ctx context.Context, b *brc20.Brc20, limit, offset int) (int, error) {
+	req := brc20.RequestTickList{
+		Limit:  limit,
+		Offset: offset,
+	}
+	resp, err := b.TickList(ctx, req)
+	if err != nil {
+		return 0, err
+	}
+
+	for _, t := range resp.Data.List {
+		fmt.Printf(
+			"\"%s\",%s,%s,%d,%s,%s,%d,%d,%s,%s\n",
+			t.Tick, t.Deployer, time.Unix(t.DeployTime, 0),
+			t.Decimals, t.Max, t.Limit, t.Holders, t.Transactions,
+			t.Minted, t.MintProgress,
+		)
+	}
+	return resp.Data.Count, nil
 }
